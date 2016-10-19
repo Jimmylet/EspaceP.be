7af77543e1e3e8b00ad927d94934b9f1e86ea253
Jimmy Letecheur <jimmy@letecheur.me>
2016-10-19 14:08:26 +0200

    create page tds french

--- wordpress/wp-content/themes/espacep/tds.php
+++ wordpress/wp-content/themes/espacep/tds.php
@@ -18,7 +18,7 @@ include 'meta.php';?>
 		
 		<div class="headintro">
 			
-			<a href="#" hreflang="#" lang="en" class="language">
+			<a href="<?php the_permalink(287); ?>" rel="alternate" hreflang="en" lang="en" class="language">
 				<span class="language__message">Click here for visit this page in English</span>
 			</a>
 			
@@ -31,6 +31,113 @@ include 'meta.php';?>
 		<div class="client-top-wrap">
 			<?php the_breadcrumb(); ?>
 		</div>
+		
+		<section class="tds-section tds-services tds-action">
+			<h2 aria-level="2" class="tds-section__title">
+				Nos actions
+			</h2>
+			<ol class="tds-section__ol">
+				<?php if( have_rows('tds-services') ):
+					while ( have_rows('tds-services') ) : the_row(); ?>
+						<li>
+							<span class="tds-section__subtitle">
+								<?php the_sub_field('titre'); ?>
+							</span>
+							<?php the_sub_field('paragraph'); ?>
+						</li>
+					<?php endwhile; endif; ?>
+			</ol>
+		</section>
+		
+		<section class="tds-section tds-droits">
+			<h2 aria-level="2" class="tds-section__title">
+				La législation sur la prostitution
+			</h2>
+			<div>
+				<?php if( have_rows('tds-droits') ):
+					while ( have_rows('tds-droits') ) : the_row(); ?>
+						<div>
+							<span class="tds-section__subtitle"><?php the_sub_field('titre'); ?></span>
+							<?php the_sub_field('paragraph'); ?>
+						</div>
+					<?php endwhile; endif; ?>
+			</div>
+		</section>
+		
+		<section class="tds-section tds-vaccins">
+			<h2 aria-level="2" class="tds-section__title">
+				Infos vaccination/dépistage
+			</h2>
+			<div>
+				<?php if( have_rows('tds-vaccins') ):
+					while ( have_rows('tds-vaccins') ) : the_row(); ?>
+						<div>
+							<span class="tds-section__subtitle"><?php the_sub_field('titre'); ?></span>
+							<?php the_sub_field('paragraph'); ?>
+						</div>
+					<?php endwhile; endif; ?>
+				<div class="tds-vaccins__ask">
+					<div class="tds-vaccins__ask__wrap">
+						<span class="tds-vaccins__ask__title">Demander le passage d’une équipe</span>
+						<?php the_field('tds-passage'); ?>
+					</div>
+					<form class="form">
+					<div class="form__top">
+						<div class="form__name">
+							<label class="form__name__label" for="name">Votre nom</label>
+							<input class="form__name__input" type="text" id="name" name="name" placeholder="Votre nom" required>
+						</div>
+						<div class="form__email">
+							<label class="form__email__label" for="email">Votre email</label>
+							<input class="form__email__input" type="email" id="email" name="email" placeholder="Votre email" required>
+						</div>
+					</div>
+					<div class="form__middle">
+						<div class="form__subject">
+							<label class="form__subject__label" for="subject">Votre sujet</label>
+							<input class="form__subject__input" type="text" id="subject" name="subject" placeholder="Le sujet de votre message" required>
+						</div>
+						<select class="select" name="carlist" form="carform">
+							<option value="volvo">Sélectionner un destinataire</option>
+							<option value="saab">Saab</option>
+							<option value="opel">Opel</option>
+							<option value="audi">Audi</option>
+						</select>
+					</div>
+					<div class="form__bottom">
+						<div class="form__message">
+							<label class="form__message__label" for="message">Votre message</label>
+							<textarea class="form__message__input" id="message" name="message" placeholder="Votre message" required></textarea>
+						</div>
+					</div>
+					<input class="form__submit" type="submit" value="Envoyer le message">
+				</form>
+				</div>
+			</div>
+		</section>
+		
+
+
+
+		<section class="tds-section tds-files">
+			<h2 aria-level="2" class="tds-section__title">
+				Documents à télécharger
+			</h2>
+			<div class="div-content">
+				<?php if( have_rows('tds-files') ):
+					while ( have_rows('tds-files') ) : the_row(); ?>
+						<div class="tds-files__wrap">
+						<?php $file = get_sub_field('tds-file'); ?>
+						<a href="<?php echo $file['url']; ?>"  alt="<?php echo $file['description']; ?>" class="tds-files__link">
+							<span class="tds-files__name"><?php the_sub_field('tds-name'); ?></span>
+							<img src="<?php echo $images; ?>/svg/pdf.svg" alt="Icone d’un fichier SVG" width="60" height="60">
+							<span class="tds-files__download">Télécharger</span>
+						</a>
+						</div>
+					<?php endwhile; endif; ?>
+			</div>
+		</section>
+		
 		
 		
 		
