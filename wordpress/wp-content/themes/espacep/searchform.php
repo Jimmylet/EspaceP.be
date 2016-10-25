ce2a0d6f4ce01a5586d1dd0aa56cfd2eb1e44f28
Jimmy Letecheur <jimmy@letecheur.me>
2016-10-25 11:33:00 +0200

    validate w3c search form

--- wordpress/wp-content/themes/espacep/searchform.php
+++ wordpress/wp-content/themes/espacep/searchform.php
@@ -1,9 +1,7 @@
 <form class="menu__search" id="searchform" action="<?php bloginfo('url'); ?>" method="get">
-	<label class="menu__search__label" for="search"><?php _e('Search:'); ?></label>
+	<label class="menu__search__label" for="s"><?php _e('Search:'); ?></label>
 	<input class="menu__search__input" type="search" required placeholder="Rechercher" value="<?php the_search_query(); ?>" name="s" id="s">
-	<button class="menu__search__submit" type="submit" name="submit"
-				 width="37"
-				 height="37" alt="Bouton de recherche">
+	<button class="menu__search__submit" type="submit" name="submit">
 				 <img src="<?php echo home_url() . '/wp-content/uploads/2016/10/search.svg'; ?>" alt="">
 	</button>
 </form>
